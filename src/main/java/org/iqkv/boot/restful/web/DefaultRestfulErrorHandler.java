724e65752a61646db9bb8050d7271386d89e6168
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2023-12-14 15:34:07 +0200

    chore: migrate to org.iqkv.boot group
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/main/java/org/iqkv/boot/restful/web/DefaultRestfulErrorHandler.java
+++ src/main/java/org/iqkv/boot/restful/web/DefaultRestfulErrorHandler.java
@@ -1,4 +1,4 @@
-package org.ujar.boot.restful.web;
+package org.iqkv.boot.restful.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
--- src/main/java/org/iqkv/boot/restful/web/RequestValidationException.java
+++ src/main/java/org/iqkv/boot/restful/web/RequestValidationException.java
@@ -1,4 +1,4 @@
-package org.ujar.boot.restful.web;
+package org.iqkv.boot.restful.web;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
