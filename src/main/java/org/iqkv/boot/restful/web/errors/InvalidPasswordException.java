d20de6e91a977ab552a4df9bee5c7eb35383138e
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-07-18 20:09:35 +0300

    feat: enable custom exceptions
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ src/main/java/org/iqkv/boot/restful/web/errors/InvalidPasswordException.java
@@ -0,0 +1,23 @@
+package org.iqkv.boot.restful.web.errors;
+
+import org.iqkv.boot.restful.web.errors.ProblemDetailWithCause.ProblemDetailWithCauseBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponseException;
+
+@SuppressWarnings("java:S110") // Inheritance tree of classes should not be too deep
+public class InvalidPasswordException extends ErrorResponseException {
+
+  private static final long serialVersionUID = 1L;
+
+  public InvalidPasswordException() {
+    super(
+        HttpStatus.BAD_REQUEST,
+        ProblemDetailWithCauseBuilder.instance()
+            .withStatus(HttpStatus.BAD_REQUEST.value())
+            .withType(ErrorConstants.INVALID_PASSWORD_TYPE)
+            .withTitle("Incorrect password")
+            .build(),
+        null
+    );
+  }
+}
