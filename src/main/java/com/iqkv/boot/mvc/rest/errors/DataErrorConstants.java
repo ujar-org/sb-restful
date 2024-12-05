aa1be513d1636fd7e38b03d361fa0ef13b3e724d
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-12-05 08:23:16 +0200

    refactor: rename ErrorConstants to DataErrorConstants

--- src/main/java/com/iqkv/boot/mvc/rest/errors/DataErrorConstants.java
+++ src/main/java/com/iqkv/boot/mvc/rest/errors/DataErrorConstants.java
@@ -2,7 +2,7 @@ package com.iqkv.boot.mvc.rest.errors;
 
 import java.net.URI;
 
-public final class ErrorConstants {
+public final class DataErrorConstants {
 
   public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
   public static final String ERR_VALIDATION = "error.validation";
@@ -10,6 +10,6 @@ public final class ErrorConstants {
   public static final URI DEFAULT_TYPE = URI.create(PROBLEM_BASE_URL + "/problem-with-message");
   public static final URI CONSTRAINT_VIOLATION_TYPE = URI.create(PROBLEM_BASE_URL + "/constraint-violation");
 
-  private ErrorConstants() {
+  private DataErrorConstants() {
   }
 }
